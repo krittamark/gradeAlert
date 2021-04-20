2d328b13c526720d5ae1c65b63e6c11db5d70f59
Krittamet Damthongkam <markkung@KrittametMBA.local>
2021-04-21 03:56:43 +0700

    Change

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     //  Url endpoint
-    endpointUrl: "http://academic.skr.ac.th/grade/skr63_1/",
+    endpointUrl: "http://academic.skr.ac.th/grade/skr63_2/",
     //  Path to alert sound file
     alertSound: "./alert.mp3",
     //  Sound's volume (0 - 1)
-    alertSoundVolume: 0.1,
+    alertSoundVolume: 0.5,
     //  Check the URL for every x seconds
     checkingTime: 1
 };
--- grade63.js
+++ grade63.js
@@ -1,14 +1,19 @@
 const config = require('./config');
-const request = require('request');
+const axios = require('axios');
 const sound = require('sound-play');
-const opn = require('opn');
+const open = require('opn');
 
 let inter = setInterval(check(config.endpointUrl, () => {
-    clearInterval(inter);
-    opn(config.endpointUrl);
-    sound.play(config.alertSound, config.alertSoundVolume);
+  clearInterval(inter);
+  open(config.endpointUrl);
+  sound.play(config.alertSound, config.alertSoundVolume);
 }), config.checkingTime * 1000);
 
+
 function check(url, success) {
-    return () => request(url, (err, res, body) => (res.statusCode === 200) ? success() : '');
+  return () => {
+    axios.get(url)
+      .then(res => success())
+      .catch(err => '');
+  }
 }
